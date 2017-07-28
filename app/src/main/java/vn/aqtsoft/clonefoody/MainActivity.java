536fa5adc6aa32f3b18d37373cd785d175d27c05
huntert20 <thanhtuan200295@gmail.com>
2017-07-28 17:24:54 +0700

    Initial commit

---
+++ app/src/main/java/vn/aqtsoft/clonefoody/MainActivity.java
@@ -0,0 +1,27 @@
+package vn.aqtsoft.clonefoody;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
