1818bc3f81469015ba8263ceac23e2a1215913f9
huntert20 <thanhtuan200295@gmail.com>
2017-10-09 17:02:36 +0700

    login screen 0.1

--- app/src/main/java/vn/aqtsoft/clonefoody/view/FlashScreenActivity.java
+++ app/src/main/java/vn/aqtsoft/clonefoody/view/FlashScreenActivity.java
@@ -1,9 +1,14 @@
 package vn.aqtsoft.clonefoody.view;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import vn.aqtsoft.clonefoody.R;
@@ -14,6 +19,7 @@ public class FlashScreenActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullScreen();
         setContentView(R.layout.activity_flashscreen);
 
         txtvVersionApp = findViewById(R.id.txtvVersionApp);
@@ -21,8 +27,28 @@ public class FlashScreenActivity extends AppCompatActivity{
         try {
             PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
             txtvVersionApp.setText(String.format("%s %s", getString(R.string.version_app), packageInfo.versionName));
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(FlashScreenActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            },2000);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void setFullScreen() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }else {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
     }
 }
--- app/src/main/java/vn/aqtsoft/clonefoody/view/LoginActivity.java
+++ app/src/main/java/vn/aqtsoft/clonefoody/view/LoginActivity.java
@@ -1,7 +1,10 @@
 package vn.aqtsoft.clonefoody.view;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import vn.aqtsoft.clonefoody.R;
 
@@ -11,5 +14,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        addViews();
+    }
+
+    private void addViews() {
+
     }
 }
